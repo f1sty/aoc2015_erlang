eb999b54d06a2dc277062813a91d3522d0a9ba64
Yurii Skrynnykov <truef1s7@gmail.com>
2023-11-08 23:34:13 +0200

    Add solutions runner to main module

--- src/aoc2015_erlang.erl
+++ src/aoc2015_erlang.erl
@@ -1,3 +1,17 @@
 -module(aoc2015_erlang).
 
--export([]).
+-export([run/0, input_filename/1]).
+
+-import(day1, [part1/0, part2/0]).
+
+input_filename(Day) ->
+    ["inputs/", Day].
+
+run() ->
+    FinishedDays = [day1, day2],
+    [run(Day) || Day <- FinishedDays],
+    ok.
+
+run(Module) ->
+    Output = [Module, [Module:Fun() || Fun <- [part1, part2]]],
+    io:format("~p: ~p~n", Output).
