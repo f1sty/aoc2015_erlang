27ea64d38f266a13bec643cc5289fbebfc99d590
Yurii Skrynnykov <truef1s7@gmail.com>
2023-11-08 23:33:32 +0200

    Add day 2 solutions

---
+++ src/day2.erl
@@ -0,0 +1,36 @@
+-module(day2).
+
+-export([part1/0, part2/0]).
+
+-import(aoc2015_erlang, [input_filename/1]).
+
+%% 2*l*w + 2*w*h + 2*h*l
+part1() ->
+    FileName = input_filename(?MODULE_STRING),
+    {ok, Binary} = file:read_file(FileName),
+    SizesRaw = binary:split(Binary, <<"\n">>, [trim_all, global]),
+    Sizes =
+        [lists:map(fun binary_to_integer/1, binary:split(S, <<"x">>, [trim_all, global]))
+         || S <- SizesRaw],
+    lists:sum(
+        lists:map(fun([Length, Width, Height] = Dimentions) ->
+                     Area = 2 * Length * Width + 2 * Width * Height + 2 * Height * Length,
+                     [A, B | _] = lists:sort(Dimentions),
+                     Area + A * B
+                  end,
+                  Sizes)).
+
+part2() ->
+    FileName = input_filename(?MODULE_STRING),
+    {ok, Binary} = file:read_file(FileName),
+    SizesRaw = binary:split(Binary, <<"\n">>, [trim_all, global]),
+    Sizes =
+        [lists:map(fun binary_to_integer/1, binary:split(S, <<"x">>, [trim_all, global]))
+         || S <- SizesRaw],
+    lists:sum(
+        lists:map(fun([Length, Width, Height] = Dimentions) ->
+                     Volume = Length * Width * Height,
+                     [A, B | _] = lists:sort(Dimentions),
+                     Volume + 2 * A + 2 * B
+                  end,
+                  Sizes)).
